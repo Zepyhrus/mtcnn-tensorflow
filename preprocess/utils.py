4021c734991888db80d977bf149cc3d8663a77de
Zepyhrus <sherkfung@gmail.com>
2019-06-16 10:50:13 +0800

    utils update

--- preprocess/utils.py
+++ preprocess/utils.py
@@ -1,21 +1,22 @@
 import numpy as np
 import os
+from os.path import join
 
 
 
 #%%
 def mkdir(dis_path):
   if not os.path.exists(dis_path):
-    os.makedirs(dis_path)
+    return os.makedirs(dis_path)
 
 #%%
 def iou(box, boxes):
   '''裁剪的box和图片所有人脸box的iou值
   参数：
-    box：裁剪的box,当box维度为4时表示box左上右下坐标，维度为5时，最后一维为box的置信度
-    boxes：图片所有人脸box,[n,4]
+  box：裁剪的box,当box维度为4时表示box左上右下坐标，维度为5时，最后一维为box的置信度
+  boxes：图片所有人脸box,[n,4]
   返回值：
-    iou值，[n,]
+  iou值，[n,]
   '''
   #box面积
   box_area = (box[2]-box[0]+1)*(box[3]-box[1]+1)
@@ -38,9 +39,9 @@ def iou(box, boxes):
 def convert_to_square(box):
   '''将box转换成更大的正方形
   参数：
-    box：预测的box,[n,5]
+  box：预测的box,[n,5]
   返回值：
-    调整后的正方形box，[n,5]
+  调整后的正方形box，[n,5]
   '''
   square_box = box.copy()
   h = box[:, 3]-box[:, 1]+1
@@ -54,3 +55,76 @@ def convert_to_square(box):
   square_box[:, 3] = square_box[:, 1]+max_side-1
   
   return square_box
+
+#%% get data from wider_face_train_bbx_gt.txt
+def read_annotation(base_dir, label_path):
+  '''读取文件的image，box'''
+  data = dict()
+  images = []
+  bboxes = []
+  labelfile = open(label_path, 'r')
+  while True:
+    # 图像地址
+    imagepath = labelfile.readline().strip('\n')
+    if not imagepath:
+      break
+    imagepath = base_dir + '/images/' + imagepath
+    images.append(imagepath)
+    # 人脸数目
+    nums = int(labelfile.readline().strip('\n'))
+    
+    if not nums:
+      nums = 1
+
+    one_image_bboxes = []
+    for i in range(nums):
+
+      bb_info = labelfile.readline().strip('\n').split(' ')
+      #人脸框
+      face_box = [float(bb_info[i]) for i in range(4)]
+
+      xmin = face_box[0]
+      ymin = face_box[1]
+      xmax = xmin + face_box[2]
+      ymax = ymin + face_box[3]
+
+      one_image_bboxes.append([xmin, ymin, xmax, ymax])
+
+    bboxes.append(one_image_bboxes)
+
+  data['images'] = images
+  data['bboxes'] = bboxes
+  return data
+
+
+#%% get data from wider_face_train.txt
+def read_anno(base_dir, label_path):
+  data = dict()
+  bboxes = []
+  images = []
+
+  f = open(label_path, 'r')
+  while True:
+    ln = f.readline()
+
+    if not ln:
+      break
+
+    ln = ln.split()
+
+    image = join(base_dir, 'images', ln[0]) + '.jpg'
+    box = [int(float(x)) for x in ln[1:]]
+
+    bbox = []
+
+    for i in range(len(ln) // 4):
+      bbox.append(box[4*i:4*(i+1)])
+
+    bboxes.append(bbox)
+    images.append(image)
+
+  f.close()
+  data['images'] = images
+  data['bboxes'] = bboxes
+  
+  return data
